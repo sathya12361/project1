5925e9d0e0274500488b8bfa368b4404586ac3aa
sathya12361 <s2516416@gmail.com>
2026-07-29 13:36:14 +0530

    base program

---
+++ base.java/JumpDemo.java
@@ -0,0 +1,12 @@
+ public class JumpDemo {
+    public static void main(String[] args) {
+        for(int i=5;i<=40;i++)
+        {
+            System.out.print(" "+i);
+            if(i<=30)
+            break;    
+              
+        }
+        System.out.println("end");
+    }       
+ }
