e19835b87d1b93d20e552f56f35f817efa5d01a9
sathya12361 <s2516416@gmail.com>
2026-07-31 20:21:06 +0530

    base program

---
+++ base.java/Demo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        int i = 0;
+        {
+            while (i < 5) {
+                System.out.println(i);
+                i++;
+                
+            }
+                
+            
+        System.out.println();
+    }
+}
+}
