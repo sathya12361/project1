6cd6aec96cd3d28c5cae9b648e0264845e03df30
sathya12361 <s2516416@gmail.com>
2026-07-31 14:28:21 +0530

    base program

--- base.java/WhilDemo.java
+++ base.java/WhilDemo.java
@@ -1,16 +1,17 @@
 import java .util.Scanner;
 public class WhilDemo {
-    public static void main(String[] args);
-    Scanner scan=new Scanner (System )
-    System.out.print(:"Enter N Value");
+    public static void main(String[] args) {
+    Scanner scan=new Scanner (System.in);
+    System.out.print("Enter N Value");
     long n=scan.nextLong();  //1234
     long r,s=0;
-    While(n!=0)  //1234 123 12 1 0
+    while (n != 0)  //1234 123 12 1 0
     {
         r=n%10;  //4  3   2   1
         n=n/10;  //123  12  1  0
-        s=s+r;  //0+4  4+3  7+2 9+
+        s=s+r;  //0+4  4+3  7+2 9+1 
     }
     System.out.println("sum of digite");
     scan.close();
+}
 }
